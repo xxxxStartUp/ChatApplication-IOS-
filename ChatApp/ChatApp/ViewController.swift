00eb3cb6bd5591cb9e943d6cd2b4083b2d88aff2
Daramfon <Daramfon@buffalo.edu>
2020-06-15 22:05:46 -0400

    New File-New Beginnings

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Daramfon Akpan on 6/15/20.
+//  Copyright © 2020 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
