49e126fb6d52d9ceb77b31899a94e71e7870059c
ebukaebunam <ebukaebunam@gmail.com>
2020-06-17 10:59:14 -0500

    added all storybords to development

--- ChatApp/Model/ebuka-FireService.swift
+++ ChatApp/Model/ebuka-FireService.swift
@@ -6,33 +6,33 @@
 //  Copyright © 2020 Daramfon Akpan. All rights reserved.
 //
 
-import Foundation
-import Firebase
-import FirebaseAuth
-
-
-extension FireService {
-    /**
-        Signs up  a user
-
-        - Parameter email:  the email to signUp with
-        - Parameter password: the passowrd of the user
-        - Returns: Void
-    */
-    func SignUp(email : String , password : String, completion : @escaping (Bool) -> ()) {
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            
-            if error != nil {
-                completion(false)
-                return
-            }
-            
-            guard result != nil else {
-                completion(true)
-                return
-            }
-        }
-    }
-    
-    
-}
+//import Foundation
+////import Firebase
+//import FirebaseAuth
+//
+//
+//extension FireService {
+//    /**
+//        Signs up  a user
+//
+//        - Parameter email:  the email to signUp with
+//        - Parameter password: the passowrd of the user
+//        - Returns: Void
+//    */
+//    func SignUp(email : String , password : String, completion : @escaping (Bool) -> ()) {
+//        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+//
+//            if error != nil {
+//                completion(false)
+//                return
+//            }
+//
+//            guard result != nil else {
+//                completion(true)
+//                return
+//            }
+//        }
+//    }
+//
+//
+//}
