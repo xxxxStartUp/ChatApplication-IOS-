8446e711d3ff439e47891abb50ec80a5a01e0a10
ebukaebunam <ebukaebunam@gmail.com>
2020-06-18 19:30:00 -0500

    working on message model and user model

---
+++ ChatApp/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by Ebuka Egbunam on 6/17/20.
+//  Copyright © 2020 Daramfon Akpan. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    
+    
+}
