a1624fa0beb8ce02d4916d3cfeb51f3159050437
Ramzi Ganou <ganouramzi@yahoo.fr>
2020-06-19 23:51:19 -0400

    Created the FireUser and Admin classes

---
+++ ChatApp/Model/FireUser.swift
@@ -0,0 +1,53 @@
+//
+//  FireUser.swift
+//  ChatApp
+//
+//  Created by Ramzi on 6/19/20.
+//  Copyright © 2020 Daramfon Akpan. All rights reserved.
+//
+
+import Foundation
+
+
+class FireUser : User {
+    
+    /*
+    Constant values for the 'id', 'name', 'email', and 'timeCreated'.
+    Variables for 'messages', 'group', 'friends', 'userType' property as they will likely change.
+     */
+    let name: String
+    let id: Int32 // I think this should be an Int as it would be easier to use an ID that is a number to quickly find or refer to a user.
+    let email: String
+    let timeCreated: Date
+    
+    var messages: [String: Any]
+    var group: [String]
+    var friends: [String]
+    var userType: Int
+    
+    
+    // When creating a user, these properties will need to be provided
+    init(userID: Int32, userName: String, userEmail: String, creationDate: Date) {
+        
+        id = userID
+        name = userName
+        email = userEmail
+        timeCreated = creationDate
+        messages = [:]
+        group = []
+        friends = []
+        userType = TypeOfUser.regular.rawValue // I created an enum where 0 would be a regular user and 1 would be an admin user.
+        super.init()
+    }
+    
+    func isAdmin() -> Bool {
+        
+        self.userType == TypeOfUser.admin.rawValue ? true:false
+    }
+}
+
+enum TypeOfUser: Int {
+    case regular = 0
+    case admin = 1
+}
+
