8489146a51eefdedad70ee826a6167ff293a5cc1
ebukaebunam <ebukaebunam@gmail.com>
2020-06-22 00:45:32 -0500

    added chat screen segue

---
+++ ChatApp/Cells/ChatCell/ChatCell-ebuka.swift
@@ -0,0 +1,18 @@
+//
+//  ChatCell-ebuka.swift
+//  ChatApp
+//
+//  Created by Ebuka Egbunam on 6/22/20.
+//  Copyright © 2020 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell_ebuka: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
